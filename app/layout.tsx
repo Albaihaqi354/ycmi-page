fb3fbf7114a33f94220789736a6d4742dbc2d951
Muhammad_Albaihaqi_Mustofa <bianalbaihaqi190@gmail.com>
2025-06-12 00:55:49 +0700

    fix ycmi web

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'YCMI | Web-Site',
+  description: 'Created by Albaihaqi',
   generator: 'v0.dev',
 }
 
